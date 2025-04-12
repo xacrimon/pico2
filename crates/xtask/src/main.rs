81648431b98a5fcf2d377230d385fea926104a92
Joel Wejdenstål <joel.wejdenstal@gmail.com>
2025-04-12 06:50:21 +0200

    Add xtask commands to start and stop a local picoremote server

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -1,5 +1,5 @@
 use std::path::PathBuf;
-use std::process;
+use std::process::{self, ExitCode};
 
 use clap::{Args, Parser, Subcommand};
 
@@ -12,6 +12,7 @@ struct App {
 #[derive(Debug, Subcommand)]
 enum Command {
     Ci(CiOptions),
+    Picoremote(PicoremoteOptions),
 }
 
 #[derive(Debug, Args)]
@@ -23,7 +24,20 @@ struct CiOptions {
     job: String,
 }
 
-fn main() {
+#[derive(Debug, Args)]
+struct PicoremoteOptions {
+    #[clap(subcommand)]
+    command: PicoremoteCommand,
+}
+
+#[derive(Debug, Subcommand)]
+enum PicoremoteCommand {
+    Build,
+    Start,
+    Stop,
+}
+
+fn main() -> ExitCode {
     let app = App::parse();
 
     match app.command {
@@ -39,5 +53,58 @@ fn main() {
                 .status()
                 .expect("act failed");
         }
+        Command::Picoremote(options) => match options.command {
+            PicoremoteCommand::Build => {
+                process::Command::new("docker")
+                    .arg("build")
+                    .arg("--tag")
+                    .arg("picoremote")
+                    .arg("./picoremote")
+                    .status()
+                    .expect("docker build failed");
+            }
+            PicoremoteCommand::Start => {
+                let inspect_status = process::Command::new("docker")
+                    .arg("inspect")
+                    .arg("xtask-picoremote-server")
+                    .arg("--format")
+                    .arg("{{.State.Running}}")
+                    .status()
+                    .expect("docker inspect failed");
+
+                if inspect_status.success() {
+                    println!(
+                        "container with name xtask-picoremote-server already exists. delete it and try again"
+                    );
+
+                    return ExitCode::FAILURE;
+                }
+
+                process::Command::new("docker")
+                    .arg("run")
+                    .arg("--name")
+                    .arg("xtask-picoremote-server")
+                    .arg("--detach")
+                    .arg("picoremote")
+                    .status()
+                    .expect("docker run failed");
+            }
+            PicoremoteCommand::Stop => {
+                process::Command::new("docker")
+                    .arg("stop")
+                    .arg("xtask-picoremote-server")
+                    .status()
+                    .expect("docker stop failed");
+
+                process::Command::new("docker")
+                    .arg("container")
+                    .arg("rm")
+                    .arg("xtask-picoremote-server")
+                    .status()
+                    .expect("docker rm failed");
+            }
+        },
     }
+
+    ExitCode::SUCCESS
 }
